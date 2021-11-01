396cb92897a6b6dad681b3541e685e5fbaa74cb0
Ihuoma <chineme4g@gmail.com>
2021-11-01 14:01:18 +0100

    Worked on Navbar

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 let menuBar = document.querySelector(".navBar");
+let mobileMenu = document.querySelector(".mobileMenu");
 let cart = document.querySelector(".cart");
 let profileImg = document.querySelector(".profile");
 
 loadEventListeners();
 
-function loadEventListeners(){
-    menuBar.addEventListener("click", displayMenu)
+function loadEventListeners() {
+  menuBar.addEventListener("click", displayMenu);
+  mobileMenu.addEventListener("click", disableBar)
 }
 
-function displayMenu(){
-    if(menuBar){
-        
+function displayMenu() {
+    if(mobileMenu.style.display== "none"){
+        mobileMenu.style.display = "block";
+        menuBar.style.display = "none"
+    }else {
+        mobileMenu.style.display = "none"
+    }
+}
+
+function disableBar(){
+    if(menuBar.style.display== "none"){
+        menuBar.style.display = "block";
+        mobileMenu.style.display = "none";
+    }else{
+        menuBar.style.display = "none"
     }
 }
