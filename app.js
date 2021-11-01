fff57c4f2227eadb248e5c2665d86abb4a00dd6c
Ihuoma <chineme4g@gmail.com>
2021-11-01 13:23:11 +0100

    added js file

---
+++ app.js
@@ -0,0 +1,15 @@
+let menuBar = document.querySelector(".navBar");
+let cart = document.querySelector(".cart");
+let profileImg = document.querySelector(".profile");
+
+loadEventListeners();
+
+function loadEventListeners(){
+    menuBar.addEventListener("click", displayMenu)
+}
+
+function displayMenu(){
+    if(menuBar){
+        
+    }
+}
