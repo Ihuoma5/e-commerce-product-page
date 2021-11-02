a0b7b54e8079f4d4ef615f8f3d67f9d81db9d751
Ihuoma <chineme4g@gmail.com>
2021-11-02 19:24:14 +0100

    Started work on cart

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ let menuBar = document.querySelector(".navBar");
 let mobileMenu = document.querySelector(".mobileMenu");
 let cart = document.querySelector(".cart");
 let profileImg = document.querySelector(".profile");
+let profileBg = document.querySelector(".profileBg");
+let cartBox = document.querySelector(".cartBox");
 
 loadEventListeners();
 
 function loadEventListeners() {
   menuBar.addEventListener("click", displayMenu);
-  mobileMenu.addEventListener("click", disableBar)
+  mobileMenu.addEventListener("click", disableBar);
+  profileBg.addEventListener("mouseover", showCart);
+  profileBg.addEventListener("mouseout", showCart);
 }
 
-function displayMenu() {
+function displayMenu(e) {
     if(mobileMenu.style.display== "none"){
         mobileMenu.style.display = "block";
         menuBar.style.display = "none"
@@ -19,11 +23,17 @@ function displayMenu() {
     }
 }
 
-function disableBar(){
+function disableBar(e){
     if(menuBar.style.display== "none"){
         menuBar.style.display = "block";
         mobileMenu.style.display = "none";
     }else{
         menuBar.style.display = "none"
     }
+}
+
+function showCart(e){
+if(cartBox.style.display = "none"){
+    cartBox.style.display = "block";
+}
 }
